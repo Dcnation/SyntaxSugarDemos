93762887147ccbdbfe69879922adff3e4f40866b
Devin Carlisle <devincarlisle@Devins-Air.attlocal.net>
2022-06-07 10:48:12 -0500

    pushing up to git

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SyntaxSugarDemos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //int answer = 4; //explicit typing
+            //string response; //explicit typing
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            // else
+            //{
+
+
+            //  response = answer + " is greater than or equal to nine";
+
+            //}
+
+            var answer = 4;
+
+            var response = (answer < 9) ? $"{answer} is less than 9" : $"{answer} is greater than or equal to nine";
+
+            Console.WriteLine(response);
+
+        }
+    }
+}
+
+
+
+
+
+
+
+        }
+    }
+}
+
